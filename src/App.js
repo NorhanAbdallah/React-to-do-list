e07ea0ac201e6e68b2135beca344979a349fa55b
Toqa Reda <tokareda26@gmail.com>
2020-02-03 14:48:08 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+    newTask: "",
+    todos: [
+      {text: 'react' , id: 0},
+      {text: 'angular' , id: 1}
+    ]
+  }
+
+  completeTask = (idm) => {
+    const todos = [...this.state.todos];
+    todos.splice(idm,1 );
+    this.setState ({
+      todos
+    }); 
+  }
+
+  addTask =() => {
+    const todos = [...this.state.todos];
+    todos.push({
+      text: this.state.newTask
+    });
+    this.setState ({
+      todos
+    }); 
+
+  }
+
+  updateNewTask = (event) => {
+    this.setState ({
+      newTask: event.target.value
+    }); 
+  }
+
+render (){
+ 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  {this.state.todos.map((todo, index) => <li key={index}>{todo.text} 
+  <button onClick= {() => this.completeTask(index)} >Done</button> </li>)}
+
+
+  <input value={this.state.newTask} onChange={this.updateNewTask}/>
+  <button onClick={this.addTask}>Add Task</button>
+  </div>
   );
+}
 }
 
 export default App;
